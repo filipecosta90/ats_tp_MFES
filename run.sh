e9297dc3e5fc41f059a0094412b7cc478dea1849
Sérgio Caldas <sergiocaldas.91@gmail.com>
2016-01-19 22:50:28 +0000

    corrected run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 EXAMPLE_DIR=exemplos/
-EXAMPLE_FILE=maiorDeDoisNumeros.i
+EXAMPLE_FILE=armstrongNumber.i
 RES_FILE=res.msp
 make clean
 make all
@@ -14,7 +14,7 @@ cp $RES_FILE ../genMaqV
 cd ../genMaqV
 javac maqv/Main.java
 echo "ended compiling maqv/Main"
-#echo "maqvMain:\n"
-#java maqv/Main $RES_FILE
+echo "maqvMain:\n"
+java maqv/Main $RES_FILE
 cd ../genI
 open metrics.txt
